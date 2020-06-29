d12443bb48d00a4dcb67fa898ba545785a6b80bf
ferhatsahin <ferhatsahin.ce@gmail.com>
2020-06-29 20:33:58 +0300

    create Pokemon Component & styling with simple css

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_POKEMONS } from '../graphql/get-pokemons';
+import { Pokemon } from '../components/Pokemon';
 
 export function PokemonsContainer() {
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-    variables: { first: 9 },
+    variables: { first: 55 },
   });
 
   return (
     <div className="container">
-      {pokemons && pokemons.map(pokemon => JSON.stringify(pokemon))}
+      {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
     </div>
   )
 }
