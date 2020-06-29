392f8f76f9bbb33402a14bdbe2a28e701824dbaa
ferhatsahin <ferhatsahin.ce@gmail.com>
2020-06-29 20:16:24 +0300

    add grapql query & pokemonContainer

---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_POKEMONS } from '../graphql/get-pokemons';
+
+export function PokemonsContainer() {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
+
+  return (
+    <div className="container">
+      {pokemons && pokemons.map(pokemon => JSON.stringify(pokemon))}
+    </div>
+  )
+}
